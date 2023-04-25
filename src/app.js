c737bd00ee12bd92bdc34ed524a2c1c51e6483ca
Breno <brennoohudson@gmail.com>
2023-04-25 17:36:01 -0300

    nodemon de volta

--- src/app.js
+++ src/app.js
@@ -8,5 +8,4 @@ app.use(cors())
 app.use(express.json());
 app.use(router)
 
-const PORT = 5000
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
+app.listen(process.env.PORT, () => console.log(`Servidor rodando na porta ${process.env.PORT}`))
